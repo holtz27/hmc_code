d6825b54973fd6ee63c5b53ba03956e483743b35
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-04-10 14:36:15 -0300

    Update normal_data.R

--- Models/normal/normal_data.R
+++ Models/normal/normal_data.R
@@ -1,7 +1,7 @@
 ############################################################################
-################################# SVM-SMN Model ############################
-# y_t = b0 + b1*y_{t-1} + b2*e(h_t) + e_t/lambda_t^{1/2}
-# h_t = mu + phi(h_{t-1} - mu) + sigma*n_{t}
+################################# SVMN Model ############################
+# y_t = b0 + b1 * y_{t-1} + b2 * e(h_t) + e_t
+# h_t = mu + phi * ( h_{t-1} - mu ) + sigma * n_{t}
 ############################################################################
 ###################### Normal: l ~ 1 
 
@@ -18,19 +18,17 @@ y = h = rep(0, T)
 #set.seed(4225873)
 for(t in 1:T){
   if(t == 1){
-    #l[t] = rgamma(1, shape = v/2, rate = v/2)
     h[t] = rnorm(1, mean = mu, sd = sigma*sqrt(1/(1-phi**2)))
     y[t] = rnorm(1, b0 + b1 * y0 + b2 * exp(h[t]), exp(h[t]/2) )
   }else{
-    #l[t] = rgamma(1, shape = v/2, rate = v/2)
     h[t] = rnorm(1, mean = (mu + phi * ( h[t-1] - mu) ), sd = sigma)
     y[t] = rnorm(1, b0 + b1*y[t - 1] + b2 * exp(h[t]), exp(h[t]/2) )
   }
 }
 
-par(mfrow = c(1, 2))
+par( mfrow = c(1, 2) )
 plot(y, type = 'l', main = 'log - retornos')
 hist(y, breaks = 40, main = 'histograma')
-par(mfrow = c(1, 1))
+par( mfrow = c(1, 1) )
 
 
