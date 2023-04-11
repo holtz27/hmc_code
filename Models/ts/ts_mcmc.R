24e3caf794a55c3fe2d55cf2210faa78fdf27a6f
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-04-11 20:43:43 -0300

    Update ts_mcmc.R

--- Models/ts/ts_mcmc.R
+++ Models/ts/ts_mcmc.R
@@ -1,6 +1,5 @@
 
 library( ggplot2 )
-
 Rcpp::sourceCpp('ts_model.cpp')
 
 # Sampling
@@ -25,13 +24,11 @@ chain_b = samples$chain$chain_b
 chain_h = samples$chain$chain_h
 chain_e = samples$chain$chain_v
 chain_l = samples$chain$chain_l
-
 # Transformations
 chain_theta[2, ] = tanh( chain_theta[2, ] )
 chain_theta[3, ] = exp( chain_theta[3, ] )
 chain_b[2, ]     = tanh( chain_b[2, ] )
 chain_v          = exp( chain_e[1, ] )
-
 ############################### Convergence analysis
 ################### Trace plots
 ### burn
@@ -58,7 +55,6 @@ N_new = length( jumps )
 ###############################################################################
 ###############################################################################
 ############################### theta
-
 par(mfrow = c(2, 3))
 plot(chain_theta[1, ], type = 'l', main = 'mu', ylab = '')
 plot(acf(chain_theta[1, ], lag.max = 100, plot = FALSE)[1:100])
@@ -70,31 +66,39 @@ plot(chain_theta[3, ], type = 'l', main = 'sigma', ylab = '')
 plot(acf(chain_theta[3, ], lag.max = 100, plot = FALSE)[1:100])
 hist(chain_theta[3, ], breaks = 40)
 par(mfrow = c(1, 1))
-
 ############### Análise numérica
 mcmcchain_theta = coda::as.mcmc( t( chain_theta ) )
 ####### Geweke Statistic
 # |G| > 1.96 evidencia não convergencia
 CD_theta = coda::geweke.diag( mcmcchain_theta )
-CD_theta
 ####### Fator de ineficiência (IF)
 # IF = N / N_eff, onde N_eff = effective Sample Size
 # Se IF >> 1, indica uma má mistura da cadeia gerada
 N_eff_theta = coda::effectiveSize( mcmcchain_theta )
 IF_theta = N_new / N_eff_theta
-IF_theta
 ####### MCMC error
 # MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_theta = round( apply( chain_theta, 
-                               MARGIN = 1, 
-                               FUN = sd) / sqrt( N_eff_theta ), 
-                        5 )
-mc_error_theta
-
+mc_error_theta = apply( chain_theta, 
+                        MARGIN = 1, 
+                        FUN = sd) / sqrt( N_eff_theta )
+                       
+theta_hat = apply( chain_theta, MARGIN = 1, FUN = mean )
+theta_min = apply( chain_theta, MARGIN = 1, FUN = quantile, probs = c(0.025) )
+theta_max = apply( chain_theta, MARGIN = 1, FUN = quantile, probs = c(0.975) )
+data_theta = matrix(
+                    c(theta_hat,
+                      theta_min,
+                      theta_max,
+                      CD_theta$z,
+                      IF_theta,
+                      mc_error_theta), nrow = 3, byrow = FALSE
+                    )
+row.names( data_theta ) = c('mu', 'phi', 'sigma')
+colnames( data_theta ) = c('média', '2.5%', '97.5%', 'CD', 'IF', 'mc_error')
+data_theta = round( data_theta, 5 )
 ###############################################################################
 ###############################################################################
 ############################### b
-
 par( mfrow = c(2, 3) )
 plot(chain_b[1, ], type = 'l', main = 'b0', ylab = '')
 plot(acf(chain_b[1, ], lag.max = 100, plot = FALSE)[1:100])
@@ -112,25 +116,34 @@ mcmcchain_b = coda::as.mcmc( t( chain_b ) )
 ####### Geweke Statistic
 # |G| > 1.96 evidencia não convergencia
 CD_b = coda::geweke.diag( mcmcchain_b )
-CD_b
 ####### Fator de ineficiência (IF)
 # IF = N / N_eff, onde N_eff = effective Sample Size
 # Se IF >> 1, indica uma má mistura da cadeia gerada
 N_eff_b = coda::effectiveSize( mcmcchain_b )
 IF_b = N_new / N_eff_b
-IF_b
 ####### MCMC error
 # MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_b = round( apply( chain_b, 
-                           MARGIN = 1, 
-                           FUN = sd) / sqrt( N_eff_b ), 
-                    5 )
-mc_error_b
+mc_error_b = apply( chain_b, 
+                    MARGIN = 1, 
+                    FUN = sd) / sqrt( N_eff_b )
 
+b_hat = apply( chain_b, MARGIN = 1, FUN = mean )
+b_min = apply( chain_b, MARGIN = 1, FUN = quantile, probs = c(0.025) )
+b_max = apply( chain_b, MARGIN = 1, FUN = quantile, probs = c(0.975) )
+data_b = matrix(
+                c(b_hat,
+                b_min,
+                b_max,
+                CD_b$z,
+                IF_b,
+                mc_error_b), nrow = 3, byrow = FALSE
+)
+row.names( data_b ) = c('b0', 'b1', 'b2')
+colnames( data_b ) = c('média', '2.5%', '97.5%', 'CD', 'IF', 'mc_error')
+data_b = round( data_b, 5 )
 ###############################################################################
 ###############################################################################
-############################### v
-
+############################### e = log( v )
 par(mfrow = c(1, 3))
 plot(chain_e, type = 'l', main = 'e = log( v )')
 abline( h = log( v ) )
@@ -143,22 +156,39 @@ mcmcchain_e = coda::as.mcmc( chain_e )
 ####### Geweke Statistic
 # |G| > 1.96 evidencia não convergencia
 CD_e = coda::geweke.diag( mcmcchain_e )
-CD_e
 ####### Fator de ineficiência (IF)
 # IF = N / N_eff, onde N_eff = effective Sample Size
 # Se IF >> 1, indica uma má mistura da cadeia gerada
 N_eff_e = coda::effectiveSize( mcmcchain_e )
 IF_e = N_new / N_eff_e
-IF_e
 ####### MCMC error
 # MCerror = sd( Variavel ) / sqrt( N_eff )
-mc_error_e = round( sd( chain_e ) / sqrt( N_eff_e ), 5 )
-mc_error_e
-
+mc_error_e = sd( chain_e ) / sqrt( N_eff_e )
+e_hat = mean( chain_e )
+e_min = quantile( chain_e, probs = c(0.025) )
+e_max = quantile( chain_e, probs = c(0.975) )
+data_e = matrix(
+                c(e_hat,
+                e_min,
+                e_max,
+                CD_e$z,
+                IF_e,
+                mc_error_e), nrow = 1, byrow = FALSE
+)
+row.names( data_e ) = c('e')
+colnames( data_e ) = c('média', '2.5%', '97.5%', 'CD', 'IF', 'mc_error')
+data_e = round( data_e, 5 )
+###############################################################################
+###############################################################################
+data = data_theta
+data = rbind( data, data_b, data_e )
+data = cbind( c(mu, phi, sigma, b0, b1, b2, log(v) ), data )
+colnames( data ) = c('vdd', 'média', '2.5%', '97.5%', 'CD', 'IF', 'mc_error')
+data = round( data, 5 )
+data
 ###############################################################################
 ###############################################################################
 ############################### l
- 
 l_hat = apply(chain_l, MARGIN = 1, FUN = mean)
 l_min = apply(chain_l, MARGIN = 1, FUN = quantile, probs = c(0.025) )
 l_max = apply(chain_l, MARGIN = 1, FUN = quantile, probs = c(0.975) )
@@ -166,8 +196,7 @@ data2 = matrix(c(1:T, l, l_hat, l_min, l_max), ncol = 5)
 data2 = data.frame(data2)
 names(data2) = c('obs', 'vdd', 'media', 'min', 'max')
 # plot1
-
-a = sample(1:T, 1)
+a = sample(1:(T - 101), 1)
 f = ggplot(data2[a:(a + 100), ]) 
 f = f + geom_line(aes(obs, media))
 f = f + geom_line(aes(obs, vdd), color = 'red')
@@ -206,16 +235,14 @@ par( mfrow = c(1,1) )
 ###############################################################################
 ###############################################################################
 ############################### h
-
 h_hat = apply(chain_h, MARGIN = 1, FUN = mean)
 h_min = apply(chain_h, MARGIN = 1, FUN = quantile, probs = c(0.025) )
 h_max = apply(chain_h, MARGIN = 1, FUN = quantile, probs = c(0.975) )
 data = matrix(c(1:T, h, h_hat, h_min, h_max), ncol = 5)
 data = data.frame(data)
 names(data) = c('obs', 'vdd', 'media', 'min','max')
-
-a = sample(1:T, 1)
-
+#plots
+a = sample(1:(T - 251), 1)
 g = ggplot(data[ a:(250 + a), ]) 
 g = g + geom_line(aes(obs, media))
 g = g + geom_line(aes(obs, vdd), color = 'red')
