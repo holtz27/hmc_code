d9895822dfed3eedddf52eab3189930fa7eeda7f
holtz27 <97683235+holtz27@users.noreply.github.com>
2023-04-19 16:47:47 -0300

    Update svm_smn_ts.h

--- Models/ts/svm_smn_ts.h
+++ Models/ts/svm_smn_ts.h
@@ -7,6 +7,7 @@ using namespace arma;
 
 mat svmnsim(vec beta, double mu, double phi, double sigma2, double y0);
 vec mvrgaussian(int n);
+vec gH_mom(mat inv_G, vec p);
 
 // theta
 // Model functions
@@ -16,10 +17,10 @@ vec glogpost_theta(vec theta, vec h, mat inv_G, mat dG_mu, mat dG_omega, mat dG_
 int G_theta(vec theta, mat &G, mat &inv_G, mat &dG_mu, mat &dG_omega, mat &dG_gamma, int T);
 // Energy functions
 vec nuH_theta(mat dG_mu, mat dG_omega, mat dG_gamma, mat inv_G, vec p);
-vec gradHmom_theta(mat inv_G, vec p);
-// hmc update
-vec rmhmc_theta(vec theta_cur, vec h, int fixp, int L, double eps, int T, int &acc);
-
+//vec gradHmom_theta(mat inv_G, vec p);
+// hmc updates
+vec hmc_theta(vec theta_cur, vec h, int L, double eps, int T, int &acc);
+vec rmhmc_theta(vec theta_cur, vec h, int fixp, int L, vec eps, int T, int &acc);
 
 // beta
 // Model functions
@@ -29,7 +30,7 @@ vec glogpost_b(vec b, vec h, vec l, mat inv_G, mat G_b0, mat dG_delta, mat dG_b2
 int G_b(vec b, vec h, vec l, mat &G, mat &inv_G, mat& dG_b0, mat &dG_delta, mat &dG_b2, int T, vec y_T);
 // Energy functions
 vec nuH_b(mat dG_b0, mat dG_delta, mat dG_b2, mat inv_G, vec p);
-vec gradHmom_b(mat inv_G, vec p);
+//vec gradHmom_b(mat inv_G, vec p);
 // hmc update
 vec rmhmc_b(vec b_cur, vec h, vec l, int fixp, int L, double eps, int T, vec y_T , int &acc);
 
